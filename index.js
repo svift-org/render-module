9569cb13b53a60fc9ff73230ea374a916c38732e
sebastian-meier <contact@sebastianmeier.eu>
2017-07-14 19:45:05 +0200

    export test

---
+++ index.js
@@ -0,0 +1,49 @@
+/*
+ *
+ * Add header info here
+ *
+ */
+
+'use strict';
+
+var renderer = {},
+  renderer.SVG = require('SVIFT-render-svg'),
+  renderBundle = require('SVIFT-render-bundle');
+
+var render = (function () {
+ 
+  var module = {},
+    rendererCount = 0,
+    rendererCCount = 0;
+
+  for(var key in renderer){ rendererCount++; }
+ 
+  /**
+  * Initiate the rendering process by sending a data object containing params, vis and data (see data/example.json for structure)
+  *
+  * @param {Object} `data` Parameters for the rendering process.
+  * @api public
+  */
+
+  module.render = function (data) {
+    for(var key in renderer){
+      renderer[key].render(data, module.renderCallback)
+    }
+  };
+
+  module.renderCallback = function () {
+    rendererCCount++;
+    if(rendererCCount >= rendererCount){
+      renderBundle.bundle(module.bundleCallback);
+    }
+  };
+
+  module.bundleCallback = function () {
+    console.log('everything done')
+  };
+ 
+  return module;
+ 
+})();
+
+module.exports = render;
